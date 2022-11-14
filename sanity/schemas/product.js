ff1e27da39014fbcc4c444352bdefa44b96a7d61
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-14 19:27:01 +0100

    adding content in product schema sanity

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -1,5 +1,17 @@
 export default {
     name: 'product',
     title: 'Product',
-    type: 'document',   
+    type: 'document',
+    fields: [
+        {
+          name: 'image',
+          title: 'Image',
+          type: 'array',
+          of: [{ type: 'image' }],
+          options: {
+            hotspot: true,
+          }
+        },
+        
+      ]   
 }
