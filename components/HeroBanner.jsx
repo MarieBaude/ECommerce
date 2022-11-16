a4205cb7bc1083776f74dfd5c9098a32f16ffd12
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-16 15:14:54 +0100

    adding btn

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const HeroBanner = () => {
   return (
-    <div className='hero-banner-container'>
+    <div className="hero-banner-container">
       <div>
-        <p className='beats-solo'>Small text</p>
+        <p className="beats-solo">Small text</p>
         <h3>TEXT</h3>
-        <img src='' alt='picture-banner-keyboard' className='hero-banner-image'/>
+        <img
+          src=""
+          alt="picture-banner-keyboard"
+          className="hero-banner-image"
+        />
         <div>
-          <Link></Link>
+          <Link href='#'>
+            <button type="button">BTN</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>text</p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
