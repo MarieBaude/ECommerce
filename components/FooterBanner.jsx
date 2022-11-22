722c04c837c82d2c73b7289e37e9a4ca5b9c43a9
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-22 20:29:12 +0100

    adding left section banner footer

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -7,12 +7,23 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
+        
         <div className="left">
             <p>{discount}</p>
             <h3>{largeText1}</h3>
             <h3>{largeText2}</h3>
             <p>{saleTime}</p>
         </div>
+
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+        
       </div>
     </div>
   );
