38965a8aadde5cbdd4abd4436b82afe7ba87d917
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-22 20:28:09 +0100

    adding text in footer

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-function FooterBanner() {
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
-        
+        <div className="left">
+            <p>{discount}</p>
+            <h3>{largeText1}</h3>
+            <h3>{largeText2}</h3>
+            <p>{saleTime}</p>
+        </div>
       </div>
     </div>
   );
