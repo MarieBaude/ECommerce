b49e480cd4dd1b32984736ac69c4af05b43953b5
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-23 22:16:34 +0100

    add content in loyout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
 import Head from 'next/head';
+
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-function Layout() {
+function Layout({ children }) {
   return (
     <div className='layout'>
       <Head>
@@ -14,7 +14,7 @@ function Layout() {
         <Navbar />
       </header>
       <main className='main-container'>
-        EMPTY
+        {children}
       </main>
       <footer>
         <Footer />
