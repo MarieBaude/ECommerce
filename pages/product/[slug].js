a5a0afaf71e5bc9923b68497aa99543496eea868
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-24 12:31:01 +0100

    adding get in slug

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,31 +1,54 @@
-import React from 'react'
-import { client, urlFor } from '../../lib/client'
+import React from "react";
+import { client, urlFor } from "../../lib/client";
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
+
   return (
     <div>
-        <div className='product-detail-container'>
-            <div>
-                <div className='image-container'>
-                    <img src='' />
-                    
-                </div>
-            </div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image && image[0])} />
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export const getStaticProps = async ({ params : { slug } }) => {
-    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-    const products = await client.fetch(query);
-  
-    const bannerQuery = '*[_type == "banner"]';
-    const bannerData = await client.fetch(bannerQuery);
-  
-    return {
-      props: { products, bannerData }
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+      slug {
+        current
+      }
     }
-  }
+    `;
 
-export default ProductDetails
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { products, product },
+  };
+};
+
+export default ProductDetails;
