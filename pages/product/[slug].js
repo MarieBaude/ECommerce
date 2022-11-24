8f35d340405214084f328dc52bddb01e85908a4b
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-24 17:43:50 +0100

    finish quantity count

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,7 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty } = useStateContext();
 
   const handleBuyNow = () => {
     onAdd(product, qty);
@@ -54,9 +55,9 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick=""><AiOutlineMinus /></span>
-              <span className="num">0</span>
-              <span className="plus" onClick=""><AiOutlinePlus /></span>
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
