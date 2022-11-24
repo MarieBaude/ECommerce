fb1c1059b2d304baebd329b5ef2dfb806a2df322
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-24 12:38:44 +0100

    adding quantity

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,10 @@
 import React from "react";
-import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 import { client, urlFor } from "../../lib/client";
 
 const ProductDetails = ({ product, products }) => {
@@ -12,25 +17,39 @@ const ProductDetails = ({ product, products }) => {
           <div className="image-container">
             <img src={urlFor(image && image[0])} />
           </div>
-
         </div>
-          <div className="product-detail-desc">
-            <h1>{name}</h1>
-            <div className="reviews">
-                <div>
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                </div>
-
-                <p>(20)</p>
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
             </div>
-            <h4>Details: </h4>
-            <p>{details}</p>
+
+            <p>(20)</p>
           </div>
 
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num">0</span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
