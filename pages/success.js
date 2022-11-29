2bcda3b6afd0c01d56bfeabac5a804198d0bacf5
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-11-29 14:25:56 +0100

    adding success page and import

---
+++ pages/success.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+
+import { useStateContext } from '../context/StateContext';
+import { runFireworks } from '../lib/utils';
+
+const Success = () => {
+  return (
+    <div>success</div>
+  )
+}
+
+export default Success
